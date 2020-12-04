83cba662130cd1beee8cf5fc7d084eefd90e734c
Sanjaay Rajkumar <sanjaay.rajkumar@hotmail.com>
2020-12-04 06:44:27 +0530

    Changed name
    I changed the name of the person who was mentioned in the issue. Line 20 was wrong.

--- chips.py
+++ chips.py
@@ -17,4 +17,4 @@ def FindPlayerName(names, playerTurn):
 # TODO #6 @Wheatenloki please finish defining the function askMove. Everything you need to make the function is there in ActualChips.cpp. Use that for reference.
 # TODO #7 @NRafa1391 please write the main code which is there in ActualChips.cpp. The code you need to write is in the int main() fuction there.
 # TODO #9 @Sakuguy please write help write the main code with @NRafa1391. Also solve all the problems in the code. You do half of the code and let Neil do the rest. The code you need to write can be reffered to from ActualChips.cpp in the int main().
-# TODO #10 @Whippingdot please help @Wheatenloki finish the function askMove.
+# TODO #10 @MaxDubs please help @Wheatenloki finish the function askMove.
