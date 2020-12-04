6650af9a1e2228139d8b452a1e5962ea0fcac635
Sanjaay Rajkumar <sanjaay.rajkumar@hotmail.com>
2020-12-04 14:48:55 +0530

    Changed some issue information.
    I changed some of the information for the issues as NRafa wants to do JS instead of Python as he doesn't know Python.
    Changed order of issues

--- chips.py
+++ chips.py
@@ -14,7 +14,8 @@ def FindPlayerName(names, playerTurn):
   else:
     return names[1]
 
-# TODO #6 @Wheatenloki please finish defining the function askMove. Everything you need to make the function is there in ActualChips.cpp. Use that for reference.
-# TODO #7 @NRafa1391 please write the main code which is there in ActualChips.cpp. The code you need to write is in the int main() fuction there.
-# TODO #9 @Sakuguy please write help write the main code with @NRafa1391. Also solve all the problems in the code. You do half of the code and let Neil do the rest. The code you need to write can be reffered to from ActualChips.cpp in the int main().
-# TODO #10 @MaxDubs please help @Wheatenloki finish the function askMove.
+# TODO #2 @MaxDubs please help @Wheatenloki finish the function askMove.
+# TODO #3 @Sakuguy please write help write the main code with @whippingdot. Also solve all the problems in the code. You do half of the code and let whippingdot do the rest. The code you need to write can be reffered to from ActualChips.cpp in the int main().
+# TODO #4 @whippingdot please write the main code which is there in ActualChips.cpp. The code you need to write is in the int main() fuction there.
+# TODO #5 @Wheatenloki please finish defining the function askMove. Everything you need to make the function is there in ActualChips.cpp. Use that for reference.
+# TODO #8 Hey, @NRafa1391 create a file on the chips branch called chips.js. Do your JS work there. I, and someone else if they want, will help you with the HTML, and if I learn CSS, then CSS too.
